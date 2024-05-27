860a63628dcbd1f299097a074bddd59462c616ef
Anudeep <anewdeep@outlook.com>
2024-05-27 20:45:44 +0530

    initial commit

---
+++ tutorial2.py
@@ -0,0 +1,9 @@
+# tuple
+programming_languages = "Python", "Java", "C++", "C#"
+
+# type(obj): data type of the python object
+print(type(programming_languages))
+
+# loops
+for language in programming_languages:
+    print(language)
