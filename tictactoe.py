6260f8e3124f76e5a288091ce4e89d49279bbcac
Anudeep <anewdeep@outlook.com>
2024-05-29 00:55:53 +0530

    added game board function

--- tictactoe.py
+++ tictactoe.py
@@ -4,8 +4,10 @@ game = [[0, 0, 0],
         [0, 0, 0],
         [0, 0, 0]]
 
-print("   0  1  2")
-count = 0
-for row in game:
-    print(count, row)
-    count += 1
+def game_board(player = 0, row = 0, column = 0):
+    print("   0  1  2")
+    game[row][column] = player
+    for count, row in enumerate(game):
+        print(count, row)
+
+game_board(player=1, row=2, column=1)
