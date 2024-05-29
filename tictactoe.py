1db337518050a0996e61448b996ad75350145377
Anudeep <anewdeep@outlook.com>
2024-05-29 18:14:03 +0530

    added error handling logic

--- tictactoe.py
+++ tictactoe.py
@@ -4,10 +4,18 @@ game = [[0, 0, 0],
         [0, 0, 0],
         [0, 0, 0]]
 
-def game_board(player = 0, row = 0, column = 0):
-    print("   0  1  2")
-    game[row][column] = player
-    for count, row in enumerate(game):
-        print(count, row)
+def game_board(game_map, player = 0, row = 0, column = 0, just_display = False):
+    try:
+        print("   0  1  2")
+        if not just_display:
+            game_map[row][column] = player
+        for count, row in enumerate(game_map):
+            print(count, row)
+        return game_map
+    except IndexError as e:
+        print("Make sure the row/column values are 0, 1 or 2\n", e)
+    except Exception as e:
+        print("Something went wrong!", e)
 
-game_board(player=1, row=2, column=1)
+game = game_board(game, just_display=True)
+game = game_board(game, player=1, row=1, column=1)
