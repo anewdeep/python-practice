55db489a9365e8f5650e9566cba07174a207a59c
Anudeep <anewdeep@outlook.com>
2024-06-10 11:34:11 +0530

    combnined code to winlogic. used itertools to cycle between players.

--- ttt_winlogic.py
+++ ttt_winlogic.py
@@ -4,6 +4,7 @@ win conditions: -
 2. vertically
 3. diagonally
 '''
+import itertools
 
 game = [[1, 0, 2],
         [1, 2, 0],
@@ -37,4 +38,37 @@ def win(current_game):
         if check.count(check[0]) == len(check) and check[0] != 0:
             print(f"Player {check[0]} is the winner vertically (|)!")
 
-win(game)
+def game_board(game_map, player = 0, row = 0, column = 0, just_display = False):
+    try:
+        print("   0  1  2")
+        if not just_display:
+            game_map[row][column] = player
+        for count, row in enumerate(game_map):
+            print(count, row)
+        return game_map
+    except IndexError as e:
+        print("Make sure the row/column values are 0, 1 or 2\n", e)
+    except Exception as e:
+        print("Something went wrong!", e)
+
+play = True
+players = [1, 2]
+
+while play:
+    game = [[0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]]
+    
+    game_won = False
+    game = game_board(game, just_display=True)
+    player_choice = itertools.cycle([1, 2])
+
+    while not game_won:
+        current_player = next(player_choice)
+        print(f"Current player: {current_player}")
+        column_choice = int(input("What column do you want to play? (0, 1, 2): "))
+        row_choice = int(input("What row do you want to play? (0, 1, 2): "))
+        game = game_board(game, current_player, row_choice, column_choice)
+ 
+# game = game_board(game, just_display=True)
+# game = game_board(game, player=1, row=1, column=1)
