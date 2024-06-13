70e23bb442c0cfef6226910904abc1be8f68a649
Anudeep <anewdeep@outlook.com>
2024-06-13 20:05:39 +0530

    finished tictactoe logic and combined code

--- ttt_winlogic.py
+++ ttt_winlogic.py
@@ -6,50 +6,64 @@ win conditions: -
 '''
 import itertools
 
-game = [[1, 0, 2],
-        [1, 2, 0],
-        [1, 2, 1]]
-
 def win(current_game):
+
+    def all_same(l):
+        if l.count(l[0]) == len(l) and l[0] != 0:
+            return True
+        else:
+            return False
+
     # Horizontal
     for row in current_game:
         print(row)
-        if row.count(row[0]) == len(row) and row[0] != 0:
+        if all_same(row):
             print(f"Player {row[0]} is the winner horizontally!")
+            return True
 
     #Diagonal
     diags = []
     for col, row in enumerate(reversed(range(len(game)))):
         diags.append(game[row][col])
-    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+    if all_same(diags):
         print(f"Player {diags[0]} is the winner diagonally (/)!")
+        return True
 
     diags = []
     for index in range(len(game)):
         diags.append(game[index][index])
-    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+    if all_same(diags):
         print(f"Player {diags[0]} is the winner diagonally (\\)!")
+        return True
 
     # Vertical
     for col in range(len(game)):
         check = []
         for row in game:
             check.append(row[col])
-        if check.count(check[0]) == len(check) and check[0] != 0:
+        if all_same(check):
             print(f"Player {check[0]} is the winner vertically (|)!")
+            return True
+        
+    return False
 
 def game_board(game_map, player = 0, row = 0, column = 0, just_display = False):
     try:
-        print("   0  1  2")
+        if game_map[row][column] != 0:
+            print("This position is occupied. Choose another!")
+            return game_map, False
+        print("   " + "  ".join([str(i) for i in range(len(game_map))]))
         if not just_display:
             game_map[row][column] = player
         for count, row in enumerate(game_map):
             print(count, row)
-        return game_map
+        return game_map, True
     except IndexError as e:
         print("Make sure the row/column values are 0, 1 or 2\n", e)
+        return game_map, False
     except Exception as e:
         print("Something went wrong!", e)
+        return game_map, False
 
 play = True
 players = [1, 2]
@@ -60,15 +74,30 @@ while play:
             [0, 0, 0]]
     
     game_won = False
-    game = game_board(game, just_display=True)
+    game, _ = game_board(game, just_display=True)
     player_choice = itertools.cycle([1, 2])
 
     while not game_won:
         current_player = next(player_choice)
         print(f"Current player: {current_player}")
-        column_choice = int(input("What column do you want to play? (0, 1, 2): "))
-        row_choice = int(input("What row do you want to play? (0, 1, 2): "))
-        game = game_board(game, current_player, row_choice, column_choice)
+        played = False
+
+        while not played:
+            column_choice = int(input("What column do you want to play? (0, 1, 2): "))
+            row_choice = int(input("What row do you want to play? (0, 1, 2): "))
+            game, played = game_board(game, current_player, row_choice, column_choice)
+
+        if win(game):
+            game_won = True
+            again = input("The game is over, would you like to play again? (y/n): ")
+            if again.lower() == "y":
+                print("restarting")
+            elif again.lower() == "n":
+                print("quitting")
+                play = False
+            else:
+                print("Now a valid answer")
+                play = False
  
 # game = game_board(game, just_display=True)
 # game = game_board(game, player=1, row=1, column=1)
